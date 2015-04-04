0eaf06340a79ff5f2a042a941ded6a6292eed568
Andrea Leopardi <an.leopardi@gmail.com>
2015-04-04 16:17:09 +0200

    Compile .po files into function clauses
    This commit lays the foundations of .po files compilation. A `__using__` macro
    has been added to the `Gettext` module in order to compile .po files and
    dynamically generate function clauses (in the caller module) in the form:
    
        def lgettext("pt_BR", "default", "Hello world") do
          "Ola mundo"
        end
    
    In order to test that everything works correctly, I had to add dummy
    applications inside test/fixtures/test_applications. These applications only
    have an `ebin/` directory (empty except for a .gitkeep file for keeping them in
    git) and translations in them.

--- lib/gettext.ex
+++ lib/gettext.ex
@@ -1,2 +1,56 @@
 defmodule Gettext do
+  alias Gettext.PO.Translation
+
+  @default_priv "priv/gettext"
+
+  @doc false
+  defmacro __using__(opts) do
+    quote bind_quoted: [opts: opts, default_priv: @default_priv] do
+      otp_app          = Keyword.fetch!(opts, :otp_app)
+      priv             = Keyword.get(opts, :priv, default_priv)
+      translations_dir = Application.app_dir(otp_app, priv)
+
+      Module.eval_quoted __MODULE__, Gettext.compile_po_files(translations_dir)
+
+      # Catchall clause.
+      def lgettext(_, _, msg) do
+        {:default, msg}
+      end
+    end
+  end
+
+  # Made public so that it can be called inside the quoted contents of the
+  # `__using__` macro.
+  @doc false
+  def compile_po_files(dir) do
+    # `true` means recursively. The last argument is the initial accumulator.
+    :filelib.fold_files(dir, "\.po$", true, &compile_po_file/2, [])
+  end
+
+  defp compile_po_file(path, acc) do
+    {locale, domain} = locale_and_domain_from_path(path)
+    translations     = Gettext.PO.parse_file!(path)
+
+    Enum.reduce translations, acc, fn
+      translation, acc -> [compile_translation(locale, domain, translation)|acc]
+    end
+  end
+
+  defp locale_and_domain_from_path(path) do
+    [file, "LC_MESSAGES", locale|_rest] = path |> Path.split |> Enum.reverse
+    domain = Path.rootname(file, ".po")
+    {locale, domain}
+  end
+
+  # Compiles a single translation into a function clause in the form:
+  #
+  #   def lgettext("it_IT", "default", "Hello world"), do: {:ok, "Ciao mondo"}
+  #
+  defp compile_translation(locale, domain, %Translation{msgid: msgid, msgstr: msgstr}) do
+    quote do
+      def lgettext(unquote(locale), unquote(domain), unquote(msgid)) do
+        {:ok, unquote(msgstr)}
+      end
+    end
+  end
 end
--- test/gettext_test.exs
+++ test/gettext_test.exs
@@ -1,8 +1,45 @@
 defmodule GettextTest do
-  use ExUnit.Case
-  doctest Gettext
+  use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  # Dynamically load dummy apps which just have an `ebin/` directory and
+  # translations.
+  for app <- ["default_priv", "custom_priv"] do
+    [__DIR__, "fixtures", "test_applications", app, "ebin"]
+    |> Path.join
+    |> Code.prepend_path
+  end
+
+  defmodule Translator do
+    use Gettext, otp_app: :default_priv
+  end
+
+  defmodule TranslatorWithCustomPriv do
+    use Gettext, otp_app: :custom_priv, priv: "translations"
+  end
+
+  test "found translations return {:ok, translation}" do
+    assert Translator.lgettext("it_IT", "default", "Hello world")
+           == {:ok, "Ciao mondo"}
+    assert Translator.lgettext("it_IT", "errors", "Invalid email address")
+           == {:ok, "Indirizzo email non valido"}
+  end
+
+  test "non-found translations return the argument message" do
+    # Unknown msgid.
+    assert Translator.lgettext("it_IT", "default", "nonexistent")
+           == {:default, "nonexistent"}
+    # Unknown domain.
+    assert Translator.lgettext("it_IT", "unknown", "Hello world")
+           == {:default, "Hello world"}
+    # Unknown locale.
+    assert Translator.lgettext("pt_BR", "nonexistent", "Hello world")
+           == {:default, "Hello world"}
+  end
+
+  test "a custom 'priv' directory can be used to store translations" do
+    assert TranslatorWithCustomPriv.lgettext("it_IT", "default", "Hello world")
+           == {:ok, "Ciao mondo"}
+    assert TranslatorWithCustomPriv.lgettext("it_IT", "errors", "Invalid email address")
+           == {:ok, "Indirizzo email non valido"}
   end
 end
