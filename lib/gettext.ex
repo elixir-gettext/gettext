3a94ef5f181dc319d7b01d483612b94ade7e5528
Andrea Leopardi <an.leopardi@gmail.com>
2015-04-04 22:38:40 +0200

    Add docs to the `Gettext` module

--- lib/gettext.ex
+++ lib/gettext.ex
@@ -1,4 +1,46 @@
 defmodule Gettext do
+  @moduledoc """
+  Provides facilities generating translation functions based on `.po` files.
+
+  In order to use the functionality provided by gettext in a module, the
+  `Gettext` module must be used in that module.
+
+  ## Options
+
+  The following is a list of options that can be passed when `Gettext` is used:
+
+    * `:otp_app` - the OTP application that contains the translation files
+      (the files with the `.po` extension).
+    * `:priv` - the directory where the translations are located. Defaults to
+      `priv/gettext`.
+
+  ## Examples
+
+  Let's say that the file at `priv/gettext/pt_BR/LC_MESSAGES/default.po` in the
+  `:my_app` OTP application contains the following:
+
+      msgid "Hello world"
+      msgstr "Ola mundo"
+
+  Then `Gettext` can be used like this:
+
+      defmodule MyApp.Gettext do
+        use Gettext, otp_app: :my_app
+      end
+
+  and the `MyApp.Gettext.lgettext/3` function will be made available:
+
+      MyApp.Gettext.lgettext("pt_BR", "default", "Hello world")
+      #=> {:ok, "Ola mundo"}
+
+  This function will default to "mirroring" the argument message if no
+  translation is found:
+
+      MyApp.Gettext.lgettext("it_IT", "default", "Hello world")
+      #=> {:default, "Hello world"}
+
+  """
+
   alias Gettext.PO.Translation
 
   @default_priv "priv/gettext"
