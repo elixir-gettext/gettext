11b82f96ef5c1851f94ea0f85986d959e742c6c5
Andrea Leopardi <an.leopardi@gmail.com>
2015-04-16 21:06:51 +0200

    Add docs to `Gettext.Interpolation`
    I added both a moduledoc and per-function docs and specs.

--- lib/gettext/interpolation.ex
+++ lib/gettext/interpolation.ex
@@ -1,4 +1,8 @@
 defmodule Gettext.Interpolation do
+  @moduledoc """
+  Provides facilities for working with interpolations.
+  """
+
   @interpolation_regex ~r/
     (?<left>)  # Start, available through :left
     %{         # Literal '%{'
@@ -7,6 +11,20 @@ defmodule Gettext.Interpolation do
     (?<right>) # End, available through :right
   /x
 
+  @doc """
+  Extracts interpolations from a given string.
+
+  This function extracts all interpolations in the form `%{interpolation}`
+  contained inside `str`, converts them to atoms and then returns a list of
+  string and interpolation keys.
+
+  ## Examples
+
+      iex> to_interpolatable("Hello %{name}, you have %{count} unread messages")
+      ["Hello ", :name, ", you have ", :count, " unread messages"]
+
+  """
+  @spec to_interpolatable(binary) :: [binary | atom]
   def to_interpolatable(str) do
     split = Regex.split(@interpolation_regex, str, on: [:left, :right], trim: true)
 
@@ -16,18 +34,61 @@ defmodule Gettext.Interpolation do
     end
   end
 
+  @doc """
+  Tells which `required` keys are missing in `bindings`.
+
+  Returns an error message which tells which keys in `required` don't appear in
+  `bindings`.
+
+  ## Examples
+
+      iex> missing_interpolation_keys(%{foo: 1}, [:foo, :bar, :baz]
+      "missing interpolation keys: bar, baz"
+
+  """
+  @spec missing_interpolation_keys(Dict.t, [atom]) :: binary
   def missing_interpolation_keys(bindings, required) do
     present = Dict.keys(bindings)
     missing = required -- present
     "missing interpolation keys: " <> Enum.map_join(missing, ", ", &to_string/1)
   end
 
+  @doc """
+  Returns all the interpolation keys contained in `str`.
+
+  This function returns a list of all the interpolation keys (patterns in the
+  form `%{interpolation}`) contained in `str`.
+
+  ## Examples
+
+      iex> keys("Hey %{name}, I'm %{other_name}")
+      [:name, :other_name]
+
+  """
+  @spec keys(binary) :: [atom]
   def keys(str) do
     str
     |> to_interpolatable
     |> Enum.filter(&is_atom/1)
   end
 
+  @doc """
+  Dynimically interpolates `str` with the given `bindings`.
+
+  This function replaces all interpolations (like `%{this}`) inside `str` with
+  the keys contained in `bindings`. It returns `{:ok, str}` if all the
+  interpolation keys in `str` are present in `bindings`, `{:error, msg}`
+  otherwise.
+
+  ## Examples
+
+      iex> interpolate "Hello %{name}", name: "José"
+      {:ok, "Hello José"}
+      iex> interpolate "%{count} errors", %{name: "Jane"}
+      {:error, "missing interpolation keys: count"}
+
+  """
+  @spec interpolate(binary, Dict.t) :: {:ok, binary} | {:error, binary}
   def interpolate(str, bindings) do
     try do
       s = Enum.map_join to_interpolatable(str), "", fn
