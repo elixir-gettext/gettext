f3ebab71743294ac14f7682fb3a54c7c0ac35c34
Andrea Leopardi <an.leopardi@gmail.com>
2015-09-10 19:13:56 +0200

    Refactor the gettext.merge task
    It's now a much "smarter" task which provides a bunch of
    functionalities. The spec for this task is roughly this:
    
    $ mix gettext.merge PO_FILE POT_FILE
    
        Both files must exist
    
    $ mix gettext.merge DIR --locale en
    
        Gets all POTs in DIR and sync with POs in DIR/LOCALE/LC_MESSAGES
    
        * If POT exists, but not PO -> create PO, fill in a couple headers
        * If PO exists, but not POT -> warn PO has no match
    
    $ mix gettext.merge DIR
    
        Gets all POTs in DIR and sync with all locales in DIR/LOCALE

--- test/mix/tasks/gettext.merge_test.exs
+++ test/mix/tasks/gettext.merge_test.exs
@@ -9,49 +9,6 @@ defmodule Mix.Tasks.Gettext.MergeTest do
     :ok
   end
 
-  test "updates existing .po files with their respective .pot file" do
-    # Running the task with no .po files does nothing and returns :noop.
-    assert Mix.Tasks.Gettext.Merge.run([], @priv_path) == :noop
-
-    # Reference .pot file with a translation that is not in the .po file yet.
-    write_file "default.pot", """
-    msgid "hello"
-    msgstr ""
-
-    msgid "world"
-    msgstr ""
-    """
-
-    write_file "it/LC_MESSAGES/default.po", """
-    msgid "hello"
-    msgstr "ciao"
-    """
-
-    # This .po file has no matching .pot file, so it's left untouched.
-    nomatch_contents = """
-    msgid "text"
-    msgstr "testo"
-    """
-    write_file "it/LC_MESSAGES/nomatch.po", nomatch_contents
-
-    output = capture_io fn ->
-      assert Mix.Tasks.Gettext.Merge.run([], @priv_path) == :ok
-    end
-
-    assert output =~ "Wrote #{Path.join(@priv_path, "it/LC_MESSAGES/nomatch.po")}"
-    assert output =~ "Wrote #{Path.join(@priv_path, "it/LC_MESSAGES/default.po")}"
-
-    assert read_file("it/LC_MESSAGES/default.po") == """
-    msgid "hello"
-    msgstr "ciao"
-
-    msgid "world"
-    msgstr ""
-    """
-
-    assert read_file("it/LC_MESSAGES/nomatch.po") == nomatch_contents
-  end
-
   defp write_file(path, contents) do
     path = Path.join(@priv_path, path)
     File.mkdir_p! Path.dirname(path)
