3763728ff35d28641640c4c0cffee7a17fa028e2
Andrea Leopardi <an.leopardi@gmail.com>
2016-04-07 12:23:06 +0200

    Handle unknown locales in Gettext.Plural
    Right now, this happens:
    
    
    
      1. A user wants to use a locale that Gettext.Plural doesn't cover,
    
         e.g., nb-no.
    
      2. They add the nb-no/LC_MESSAGES/*.po translation files, hence...
    
      3. ...a nb-no clause is generated for the l(n)gettext functions in
    
         each backend.
    
      4. This clause is hit when translating nb-no messages, so we call
    
         Gettext.Plural.plural("nb-no", n) (if Gettext.Plural is the chosen
    
         pluralizer).
    
      5. Gettext.Plural doesn't cover "nb-no", thus function clause error.
    
    
    
    We decided to add a catch-all clause to the functions in Gettext.Plural
    
    to handle such cases; while doing that, we also decided to stop handling
    
    locales such as `"en" <> _` and start handling them in a more controlled
    
    way. Now, `"en"` and `"en_US"` match but `"enfoo"` doesn't.

--- test/gettext/plural_test.exs
+++ test/gettext/plural_test.exs
@@ -1,6 +1,7 @@
 defmodule Gettext.PluralTest do
   use ExUnit.Case, async: true
   alias Gettext.Plural
+  alias Gettext.Plural.UnknownLocaleError
 
   test "x_* locales are pluralized like x except for exceptions" do
     assert Plural.nplurals("en") == Plural.nplurals("en_GB")
@@ -26,5 +27,22 @@ defmodule Gettext.PluralTest do
     assert Plural.plural("it", 1) == 0
     assert Plural.plural("it", 0) == 1
     assert Plural.plural("it", 100) == 1
+  end
+
+  test "locale with a country" do
+    # The _XX in en_XX gets stripped and en_XX is pluralized as en.
+    assert Plural.nplurals("en_XX") == Plural.nplurals("en")
+    assert Plural.plural("en_XX", 100) == Plural.plural("en", 100)
+  end
+
+  test "unknown locale" do
+    message = ~r/unknown locale "wat"/
+    assert_raise UnknownLocaleError, message, fn -> Plural.nplurals("wat") end
+    assert_raise UnknownLocaleError, message, fn -> Plural.plural("wat", 1) end
+
+    # This happens with dash as the country/locale separator
+    # (https://en.wikipedia.org/wiki/IETF_language_tag).
+    message = ~r/unknown locale "en-us"/
+    assert_raise UnknownLocaleError, message, fn -> Plural.nplurals("en-us") end
   end
 end
