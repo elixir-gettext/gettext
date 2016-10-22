b50c5e35254fbddbd1bb1bfd2be4074c5de94634
Andrea Leopardi <an.leopardi@gmail.com>
2016-10-22 20:58:40 +0200

    Bump version to v0.12.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Gettext.Mixfile do
   use Mix.Project
 
-  @version "0.12.0"
+  @version "0.12.1"
 
   @description "Internationalization and localization through gettext"
   @repo_url "https://github.com/elixir-lang/gettext"
