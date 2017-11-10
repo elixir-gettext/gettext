e2dbc645fcd1af3c59d5a7662b0ad978db6fbfe8
Andrea Leopardi <an.leopardi@gmail.com>
2017-11-10 15:33:43 -0500

    Improve the style of mix.exs

--- mix.exs
+++ mix.exs
@@ -7,37 +7,48 @@ defmodule Gettext.Mixfile do
   @repo_url "https://github.com/elixir-lang/gettext"
 
   def project do
-    [app: :gettext,
-     version: @version,
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     deps: deps(),
+    [
+      app: :gettext,
+      version: @version,
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      deps: deps(),
 
-     # Hex
-     package: hex_package(),
-     description: @description,
+      # Hex
+      package: hex_package(),
+      description: @description,
 
-     # Docs
-     name: "gettext",
-     docs: [source_ref: "v#{@version}", main: "Gettext",
-            source_url: @repo_url]]
+      # Docs
+      name: "gettext",
+      docs: [
+        source_ref: "v#{@version}",
+        main: "Gettext",
+        source_url: @repo_url
+      ]
+    ]
   end
 
   def application do
-    [applications: [:logger],
-     env: [default_locale: "en"],
-     mod: {Gettext.Application, []}]
+    [
+      applications: [:logger],
+      env: [default_locale: "en"],
+      mod: {Gettext.Application, []}
+    ]
   end
 
   def hex_package do
-    [maintainers: ["Andrea Leopardi", "José Valim"],
-     licenses: ["Apache 2.0"],
-     links: %{"GitHub" => @repo_url},
-     files: ~w(lib src/gettext_po_parser.yrl mix.exs *.md)]
+    [
+      maintainers: ["Andrea Leopardi", "José Valim"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => @repo_url},
+      files: ~w(lib src/gettext_po_parser.yrl mix.exs *.md)
+    ]
   end
 
   defp deps do
-    [{:earmark, ">= 0.0.0", only: :docs},
-     {:ex_doc, ">= 0.0.0", only: :docs}]
+    [
+      {:earmark, ">= 0.0.0", only: :docs},
+      {:ex_doc, ">= 0.0.0", only: :docs}
+    ]
   end
 end
