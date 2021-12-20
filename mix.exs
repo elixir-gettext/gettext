c36152c84604ed2c3bd8968a85911dfe616068a4
Andrea Leopardi <an.leopardi@gmail.com>
2021-12-20 10:04:20 +0100

    Release 0.19.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Gettext.Mixfile do
   use Mix.Project
 
-  @version "0.18.2"
+  @version "0.19.0"
 
   @description "Internationalization and localization through gettext"
   @repo_url "https://github.com/elixir-gettext/gettext"
