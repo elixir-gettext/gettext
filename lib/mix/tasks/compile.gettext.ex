80a90ae463dbd6bd7b4497ce775afe7c7e7a9377
José Valim <jose.valim@dashbit.co>
2022-07-20 18:44:13 +0200

    Start v0.21-dev

--- lib/mix/tasks/compile.gettext.ex
+++ lib/mix/tasks/compile.gettext.ex
@@ -2,14 +2,13 @@ defmodule Mix.Tasks.Compile.Gettext do
   @moduledoc false
 
   def run(_args) do
-    IO.warn """
+    IO.warn("""
     the :gettext compiler is no longer required in your mix.exs.
 
     Please find the following line in your mix.exs and remove the :gettext entry:
 
         compilers: [..., :gettext, ...] ++ Mix.compilers(),
-
-    """
+    """)
 
     {:noop, []}
   end
