97758f299e2c0e591f0df702b1983be27f1bc737
Andrea leopardi <an.leopardi@gmail.com>
2023-01-21 09:01:43 +0100

    Improve docs and specs for interpolation stuff

--- lib/gettext/interpolation.ex
+++ lib/gettext/interpolation.ex
@@ -1,23 +1,44 @@
 defmodule Gettext.Interpolation do
   @moduledoc """
-  Behaviour to provide Gettext String Interpolation.
+  Behaviour to provide Gettext string interpolation.
+
+  By default, Gettext uses `Gettext.Interpolation.Default` as the interpolation module.
   """
 
-  @type translation_type :: :translation | :plural_translation
+  @typedoc since: "0.19.0"
+  @type translation_type() :: :translation | :plural_translation
 
-  @callback runtime_interpolate(message :: String.t(), bindings :: map) ::
-              {:ok, String.t()}
-              | {:missing_bindings, message :: String.t(), missing_bindings :: [atom]}
-
-  @macrocallback compile_interpolate(
-                   translation_type :: translation_type,
-                   message :: String.t(),
-                   bindings :: map()
-                 ) :: Macro.t()
+  @typedoc since: "0.22.0"
+  @type bindings() :: %{optional(atom()) => term()}
 
   @doc """
-  Defines the gettext message format
-  https://www.gnu.org/software/gettext/manual/html_node/PO-Files.html#index-msgstr
+  Called to perform interpolation *at runtime*.
+
+  If successful, should return `{:ok, interpolated_string}`. If there
+  are missing bindings, should return `{:missing_bindings, partially_interpolated, missing}`
+  where `partially_interpolated` is a string with the available bindings interpolated.
   """
-  @callback message_format :: String.t()
+  @doc since: "0.19.0"
+  @callback runtime_interpolate(message :: String.t(), bindings()) ::
+              {:ok, String.t()}
+              | {:missing_bindings, partially_interpolated_message :: String.t(),
+                 missing_bindings :: [atom()]}
+
+  @doc """
+  Called to perform interpolation *at compile time*.
+  """
+  @doc since: "0.19.0"
+  @macrocallback compile_interpolate(translation_type(), message :: String.t(), bindings()) ::
+                   Macro.t()
+
+  @doc """
+  Defines the Gettext message format to be used when extracting.
+
+  The default interpolation module that ships with Gettext uses `"elixir-format"`.
+
+  See the [GNU Gettext
+  documentation](https://www.gnu.org/software/gettext/manual/html_node/PO-Files.html#index-msgstr).
+  """
+  @doc since: "0.19.0"
+  @callback message_format() :: String.t()
 end
