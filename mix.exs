4a096d983c66b25c70719b06f9abcf4bbbe54b3d
Andrea Leopardi <an.leopardi@gmail.com>
2023-08-16 14:26:16 +0200

    Go back to v0.24.0-dev in mix.exs

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Gettext.Mixfile do
   use Mix.Project
 
-  @version "0.23.1"
+  @version "0.24.0-dev"
 
   @description "Internationalization and localization through gettext"
   @repo_url "https://github.com/elixir-gettext/gettext"
